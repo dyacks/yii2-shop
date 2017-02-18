6cc1e576af896520ca1cbbe20a728e1e04add0f2
Dyachuk Sergey <dyachukSerge@gmail.com>
2017-02-18 19:54:44 +0200

    add pagination category

--- views/category/view.php
+++ views/category/view.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use yii\widgets\linkPager;
 ?>
 
 <section id="advertisement">
@@ -94,18 +95,22 @@ use yii\helpers\Html;
                                 <div class="clearfix"></div>
                             <?php endif; ?>
                         <?php endforeach; ?>
+                        <div class="clearfix"></div>
+                        <?php
+                            echo LinkPager::widget([
+                                'pagination' => $pages,
+                            ]);
+                        ?>
                         <?php else: ?>
                         <h2>Товары не найдены</h2>
                     <?php endif; ?>
 
-                    <div class="clearfix"></div>
-
-                    <ul class="pagination">
-                        <li class="active"><a href="">1</a></li>
-                        <li><a href="">2</a></li>
-                        <li><a href="">3</a></li>
-                        <li><a href="">&raquo;</a></li>
-                    </ul>
+<!--                    <ul class="pagination">-->
+<!--                        <li class="active"><a href="">1</a></li>-->
+<!--                        <li><a href="">2</a></li>-->
+<!--                        <li><a href="">3</a></li>-->
+<!--                        <li><a href="">&raquo;</a></li>-->
+<!--                    </ul>-->
                 </div><!--features_items-->
             </div>
         </div>
